d9e3e318dcb749f77a796eeda6a4817db109b49f
Henristote <gicquel.henri@gmail.com>
2025-04-21 22:35:17 +0200

    :zap: Fix Matrix

--- Mathy/matrix2x2.py
+++ Mathy/matrix2x2.py
@@ -12,10 +12,14 @@ class Matrix_2x2:
 		else : 
 			raise Error('{matrix2} is not a matrix')
 
-	def prod_r (self, k):
-		for i in range(0,2):
-			for j in range(0,2):
-				self.matrix[i][j] = self.matrix[i][j] * k
+	def prod_r (self, x):
+		if (isinstance(matrix2, Matrix_2x2)):
+			res = [[0,0][0,0]]
+			for i in range(0,2):
+				for j in range(0,2):
+					res[i][j] = self.matrix[i][j] * x
+		else: 
+			raise Error('{matrix2} is not a matrix')
 
 	def prod (self,matrix2):
 		if (isinstance(matrix2, Matrix_2x2)):
@@ -23,6 +27,6 @@ class Matrix_2x2:
 			x2 = (self.matrix[0][0] * matrix2[0][1]) + (self.matrix[0][1] * matrix2[1][1])
 			x3 = (self.matrix[1][0] * matrix2[0][0]) + (self.matrix[1][1] * matrix2[1][0])
 			x4 = (self.matrix[1][0] * matrix2[0][1]) + (self.matrix[1][1] * matrix2[1][1])
-		return [[x1,x2],[x3,x4]]
+			return [[x1,x2],[x3,x4]]
 		else: 
 			raise Error('{matrix2} is not a matrix')
