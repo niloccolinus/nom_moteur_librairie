a0fd366ccb0ec4b2350fe7415a93d3012aeb9833
niloccolinus <niloccolin@protonmail.com>
2025-04-25 23:22:36 +0200

    :recycle: Rename Vector2 to respect naming conventions and update Matrix2x2 reference

--- Mathy/Vector2.py
+++ Mathy/Vector2.py
@@ -1,7 +1,7 @@
-from matrix2x2 import Matrix_2x2
+from matrix2x2 import Matrix2x2
 
 
-class Vector_2x2:
+class Vector2:
     def __init__(self, x: float | int, y: float | int):
         self.x, self.y = x, y
         self.vector = [self.x, self.y]
@@ -23,44 +23,44 @@ class Vector_2x2:
         return self.norm
 
     def add(self, vector_2):
-        if (isinstance(vector_2, Vector_2x2)):
-            result = Vector_2x2(0, 0)
+        if (isinstance(vector_2, Vector2)):
+            result = Vector2(0, 0)
             result.set_x(self.x + vector_2.x)
             result.set_y(self.y + vector_2.y)
             return result.vector
         else:
-            raise TypeError('{vector_2} is not a vector')
+            raise TypeError('{vector_2} is not a Vector2')
 
     def substract(self, vector_2):
-        if (isinstance(vector_2, Vector_2x2)):
-            result = Vector_2x2(0, 0)
+        if (isinstance(vector_2, Vector2)):
+            result = Vector2(0, 0)
             result.set_x(self.x - vector_2.x)
             result.set_y(self.y - vector_2.y)
             return result.vector
         else:
-            raise TypeError('{vector_2} is not a vector')
+            raise TypeError('{vector_2} is not a Vector2')
 
     def multiply_by_scalar(self, factor: float | int):
-        result = Vector_2x2(0, 0)
+        result = Vector2(0, 0)
         result.set_x(self.x * factor)
         result.set_y(self.y * factor)
         return result.vector
 
     def scalar_product(self, vector_2) -> int | float:
-        if (isinstance(vector_2, Vector_2x2)):
+        if (isinstance(vector_2, Vector2)):
             result = self.x * vector_2.x + self.y * vector_2.y
             return result
         else:
-            raise TypeError('{vector_2} is not a vector')
+            raise TypeError('{vector_2} is not a Vector2')
 
-    def multiply_by_square_matrix(self, matrix: Matrix_2x2):
-        if (isinstance(matrix, Matrix_2x2)):
-            result = Vector_2x2(0, 0)
+    def multiply_by_square_matrix(self, matrix: Matrix2x2):
+        if (isinstance(matrix, Matrix2x2)):
+            result = Vector2(0, 0)
             result.set_x(self.x * matrix.x1 + self.y * matrix.x2)
             result.set_y(self.x * matrix.x3 + self.y * matrix.x4)
             return result.vector
         else:
-            raise TypeError('{matrix} is not a matrix')
+            raise TypeError('{matrix} is not a Matrix2x2')
 
     # To do:
     # ajouter méthode changement de base
