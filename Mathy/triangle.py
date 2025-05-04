6e98fe2f05c3b8e0281714d7eff8e12f74e0f885
niloccolinus <niloccolin@protonmail.com>
2025-05-04 21:38:21 +0200

    :sparkles: Add side length method

--- Mathy/triangle.py
+++ Mathy/triangle.py
@@ -1,3 +1,5 @@
+import math
+
 class Triangle:
     def __init__(self, p1: tuple[float, float], p2: tuple[float, float], p3: tuple[float, float]):
         """
@@ -7,3 +9,20 @@ class Triangle:
         self.p1 = p1
         self.p2 = p2
         self.p3 = p3
+
+    def side_lengths(self) -> tuple[float, float, float]:
+        """
+        Return the lengths of the triangle's sides (a, b, c) where:
+            a = length between p1 and p2
+            b = length between p2 and p3
+            c = length between p1 and p3
+        """
+        def distance(pA, pB):
+            dx = pA[0] - pB[0]
+            dy = pA[1] - pB[1]
+            return math.sqrt(dx ** 2 + dy ** 2)
+
+        a = distance(self.p1, self.p2)
+        b = distance(self.p2, self.p3)
+        c = distance(self.p1, self.p3)
+        return a, b, c
