b724f7d499e04ade07706acc602768709fbf7b52
Leïla P. <leila.karukera@gmail.com>
2025-05-08 21:57:14 +0200

    ✏️ Fix typos in test_vector.py

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -32,30 +32,30 @@ def test_add():
     assert Vector2(0.5, -1.0).add(Vector2(3.5, 5.0)) == Vector2(4.0, -2.0)
 
     # Add wrong type
-    with pytest.raises(TypeError, match=r".* is not a Vector2 .*"):
+    with pytest.raises(TypeError, match=r".* is not a Vector2.*"):
         Vector2(1, 2).add(-4)
 
 
-def test_substract():
-    # Substract zeros
-    assert Vector2(0, 0).substract(Vector2(0, 0)) == Vector2(0, 0)
+def test_subtract():
+    # Subtract zeros
+    assert Vector2(0, 0).subtract(Vector2(0, 0)) == Vector2(0, 0)
 
-    # Substract ints
-    assert Vector2(1, 2).substract(Vector2(3, -4)) == Vector2(-3, 6)
+    # Subtract ints
+    assert Vector2(1, 2).subtract(Vector2(3, -4)) == Vector2(-3, 6)
 
-    # Substract floats
-    assert Vector2(0.5, -1.0).substract(Vector2(3.5, 5.0)) == Vector2(-3.0, -6.0)
+    # Subtract floats
+    assert Vector2(0.5, -1.0).subtract(Vector2(3.5, 5.0)) == Vector2(-3.0, -6.0)
 
-    # Substract wrong type
-    with pytest.raises(TypeError, match=r".* is not a Vector2 .*"):
-        Vector2(1, 2).substract(-4)
+    # Subtract wrong type
+    with pytest.raises(TypeError, match=r".* is not a Vector2.*"):
+        Vector2(1, 2).subtract(-4)
 
 
 def test_scalar_product():
     # Scalar product of v1 = [1, 1] and v2 = [1, -1]
     assert Vector2(1, 1).scalar_product(Vector2(1, -1)) == 0
     # Use wrong type
-    with pytest.raises(TypeError, match=r".* is not a Vector2 .*"):
+    with pytest.raises(TypeError, match=r".* is not a Vector2.*"):
         Vector2(1, 1).scalar_product(0)
 
 
@@ -72,7 +72,7 @@ def test_multiply_by_matrix():
     # Multiply by matrix
     assert Vector2(3, -4).multiply_by_matrix(Matrix2x2(2, 3, 4, -1)) == Vector2(-6, 16)
     # Multiply by wrong type
-    with pytest.raises(TypeError, match=r".* is not a Matrix2x2 .*"):
+    with pytest.raises(TypeError, match=r".* is not a Matrix2x2.*"):
         Vector2(3, -4).multiply_by_matrix(Vector2(2, 3))
 
 
@@ -85,5 +85,5 @@ def test_normalize():
     # Normalize vector
     assert Vector2(1, 0).normalize == Vector2(1, 0)
     # Norm == 0
-    with pytest.raises(ValueError, match=r".* Cannot normalize a zero vector .*"):
+    with pytest.raises(ValueError, match=r".* Cannot normalize a zero vector.*"):
         Vector2(0, 0).normalize
