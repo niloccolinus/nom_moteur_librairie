6b9d3d9c35727fbc3a27861277c1f6773bf650ec
Leïla P. <leila.karukera@gmail.com>
2025-05-09 15:28:09 +0200

    Update __init__.py

--- Mathy/__init__.py
+++ Mathy/__init__.py
@@ -1,1 +1,8 @@
 """Marks the current directory as a package."""
+
+from .renderer import Renderer
+from .vector2 import Vector2
+from .triangle import Triangle
+from .matrix2x2 import Matrix2x2
+
+__all__ = ["Renderer", "Vector2", "Triangle", "Matrix2x2"]
