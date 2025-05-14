dfd4911a0c8eea3a4fefa9d5b1e41cd48e58e50f
Leïla P. <leila.karukera@gmail.com>
2025-05-14 16:35:41 +0200

    🔧 Add new Mathy classes and utils to __init__.py

--- Mathy/__init__.py
+++ Mathy/__init__.py
@@ -5,5 +5,21 @@ from .vector2 import Vector2
 from .triangle import Triangle
 from .matrix2x2 import Matrix2x2
 from .vector3 import Vector3
+from .matrix3x3 import Matrix3x3
+from .matrix3x3 import TranslationMatrix3x3
+from .math_utils import pi, factorial, deg, sin, cos
 
-__all__ = ["Renderer", "Vector2", "Triangle", "Matrix2x2", "Vector3"]
+__all__ = [
+    "Renderer",
+    "Vector2",
+    "Triangle",
+    "Matrix2x2",
+    "Matrix3x3",
+    "Vector3",
+    "TranslationMatrix3x3",
+    "pi",
+    "factorial",
+    "deg",
+    "sin",
+    "cos"
+]
