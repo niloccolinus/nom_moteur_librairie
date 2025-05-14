2299afcb159b0e7ee916ee04906545cf03e37e6a
niloccolinus <niloccolin@protonmail.com>
2025-05-14 16:51:09 +0200

    ♻️ Update init with rotation and homothety imports

--- Mathy/__init__.py
+++ Mathy/__init__.py
@@ -4,8 +4,10 @@ from .renderer import Renderer
 from .vector2 import Vector2
 from .triangle import Triangle
 from .matrix2x2 import Matrix2x2
-from .matrix3x3 import Matrix3x3
+from .matrix3x3 import Matrix3x3 
 from .matrix3x3 import TranslationMatrix3x3
+from .matrix3x3 import RotationMatrix3x3
+from .matrix3x3 import HomothetyMatrix3x3
 from .math_utils import pi, factorial, deg, sin, cos
 
 __all__ = [
@@ -15,6 +17,8 @@ __all__ = [
     "Matrix2x2", 
     "Matrix3x3", 
     "TranslationMatrix3x3",
+    "RotationMatrix3x3",
+    "HomothetyMatrix3x3",
     "pi",
     "factorial",
     "deg",
