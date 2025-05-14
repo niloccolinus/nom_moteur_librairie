0bb5c93b1084b5e54fbb9beef61e62351fd2a886
niloccolinus <niloccolin@protonmail.com>
2025-05-14 15:46:36 +0200

    ♻️ Update Mathy init imports

--- Mathy/__init__.py
+++ Mathy/__init__.py
@@ -4,5 +4,20 @@ from .renderer import Renderer
 from .vector2 import Vector2
 from .triangle import Triangle
 from .matrix2x2 import Matrix2x2
+from .matrix3x3 import Matrix3x3
+from .matrix3x3 import TranslationMatrix3x3
+from .math_utils import pi, factorial, deg, sin, cos
 
-__all__ = ["Renderer", "Vector2", "Triangle", "Matrix2x2"]
+__all__ = [
+    "Renderer", 
+    "Vector2", 
+    "Triangle", 
+    "Matrix2x2", 
+    "Matrix3x3", 
+    "TranslationMatrix3x3",
+    "pi",
+    "factorial",
+    "deg",
+    "sin",
+    "cos"
+]
