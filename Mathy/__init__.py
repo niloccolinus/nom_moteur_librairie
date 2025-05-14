79ce4055ffac07e899953935fb596263681b48b6
Leïla P. <leila.karukera@gmail.com>
2025-05-14 16:22:11 +0200

    🔧 Add Vector3 import to __init__.py

--- Mathy/__init__.py
+++ Mathy/__init__.py
@@ -4,5 +4,6 @@ from .renderer import Renderer
 from .vector2 import Vector2
 from .triangle import Triangle
 from .matrix2x2 import Matrix2x2
+from .vector3 import Vector3
 
-__all__ = ["Renderer", "Vector2", "Triangle", "Matrix2x2"]
+__all__ = ["Renderer", "Vector2", "Triangle", "Matrix2x2", "Vector3"]
